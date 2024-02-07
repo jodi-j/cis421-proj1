48afcbfbd2926e04c09801d660a878b389cf6d75
shams-ahson <sahson456@gmail.com>
2024-02-06 23:20:05 -0500

    implementing class

--- grid_maze.py
+++ grid_maze.py
@@ -4,8 +4,37 @@
 #BFSm.run()
 
 import random
+from collections import deque
 
-def create_maze():
+class Maze:
+    def __init__(self, size, density):
+        self.size = size
+        self.density = density
+        self.maze = [['.' for _ in range(size)] for _ in range(size)]
+        self.start = self.maze[0][0] = 'S'
+
+    def create_maze(self):
+        self.start = 'S'
+        goal_row, goal_col = random.randint(0, 9), random.randint(0, 9)
+        self.maze[goal_row][goal_col] = 'G'
+
+        num_obstacles = random.randint(10, 12)
+        for _ in range(num_obstacles):
+            obstacle_row, obstacle_col = random.randint(0, 9), random.randint(0, 9)
+            while self.maze[obstacle_row][obstacle_col] != '.':
+                obstacle_row, obstacle_col = random.randint(0, 9), random.randint(0, 9)
+            self.maze[obstacle_row][obstacle_col] = 'x'
+        return self.maze
+    
+    def print_maze(self):
+        for row in self.maze:
+            print(' '.join(row))
+
+test = Maze(size=10, density=0.3)
+test.create_maze()
+test.print_maze()
+
+'''def create_maze():
     maze = [['.' for _ in range(10)] for _ in range(10)]
     
     # Place the start state 's' in the top-left corner
@@ -29,8 +58,27 @@ def print_maze(maze):
     for row in maze:
         print(' '.join(row))
 
+def breadth_first_search(problem):
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):
+        return node
+    frontier = deque([node])
+    explored = set()
+    while frontier:
+        node = frontier.popleft()
+        explored.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                if problem.goal_test(child.state):
+                    return child
+                frontier.append(child)
+    return None
+
+
+
+
 maze = create_maze()
-print_maze(maze)
+print_maze(maze)'''
 
 
 
