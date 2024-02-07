dafdca238aa776becaf68825f55713dfefbd1bf9
hudahussaini <hudah@umich.edu>
2024-02-07 11:03:32 -0500

    bfs

--- grid_maze.py
+++ grid_maze.py
@@ -1,8 +1,3 @@
-#from pyamaze import maze
-#BFSm=maze()
-#BFSm.CreateMaze(loopPercent=100)
-#BFSm.run()
-
 import random
 from collections import deque
 
@@ -11,74 +6,58 @@ class Maze:
         self.size = size
         self.density = density
         self.maze = [['.' for _ in range(size)] for _ in range(size)]
-        self.start = self.maze[0][0] = 'S'
+        self.start = (0, 0)  # Store start position as a tuple (row, column)
+        self.goal = None  # Will store the goal position once created
 
     def create_maze(self):
-        self.start = 'S'
-        goal_row, goal_col = random.randint(0, 9), random.randint(0, 9)
+        #self.maze[0][0] = 'S'
+        goal_row, goal_col = random.randint(0, self.size - 1), random.randint(0, self.size - 1)
+        self.goal = (goal_row, goal_col)
         self.maze[goal_row][goal_col] = 'G'
 
         num_obstacles = random.randint(10, 12)
         for _ in range(num_obstacles):
-            obstacle_row, obstacle_col = random.randint(0, 9), random.randint(0, 9)
+            obstacle_row, obstacle_col = random.randint(0, self.size - 1), random.randint(0, self.size - 1)
             while self.maze[obstacle_row][obstacle_col] != '.':
-                obstacle_row, obstacle_col = random.randint(0, 9), random.randint(0, 9)
+                obstacle_row, obstacle_col = random.randint(0, self.size - 1), random.randint(0, self.size - 1)
             self.maze[obstacle_row][obstacle_col] = 'x'
+
         return self.maze
     
     def print_maze(self):
         for row in self.maze:
             print(' '.join(row))
 
+    def breadth_first_search(self):
+        frontier = deque([(self.start, [])])  # Initialize the queue with the start position and an empty path
+        explored = set()  # Keep track of visited positions
+
+        while frontier:
+            current, path = frontier.popleft()
+            row, col = current
+
+            if current == self.goal:
+                return path + [current]  # Return the path when the goal is reached
+
+            if 0 <= row < self.size and 0 <= col < self.size and self.maze[row][col] == '.' and current not in explored:
+                explored.add(current)
+                frontier.append(((row + 1, col), path + [current]))
+                frontier.append(((row - 1, col), path + [current]))
+                frontier.append(((row, col + 1), path + [current]))
+                frontier.append(((row, col - 1), path + [current]))
+
+        return None
+
 test = Maze(size=10, density=0.3)
 test.create_maze()
 test.print_maze()
 
-'''def create_maze():
-    maze = [['.' for _ in range(10)] for _ in range(10)]
-    
-    # Place the start state 's' in the top-left corner
-    maze[0][0] = 'S'
-    
-    # Place the goal state 'g' at a random location
-    goal_row, goal_col = random.randint(0, 9), random.randint(0, 9)
-    maze[goal_row][goal_col] = 'G'
-    
-    # Place a random number of obstacles 'o' in random locations
-    num_obstacles = random.randint(10, 12)
-    for _ in range(num_obstacles):
-        obstacle_row, obstacle_col = random.randint(0, 9), random.randint(0, 9)
-        while maze[obstacle_row][obstacle_col] != '.':
-            obstacle_row, obstacle_col = random.randint(0, 9), random.randint(0, 9)
-        maze[obstacle_row][obstacle_col] = 'x'
-    
-    return maze
+path = test.breadth_first_search()
 
-def print_maze(maze):
-    for row in maze:
-        print(' '.join(row))
-
-def breadth_first_search(problem):
-    node = Node(problem.initial)
-    if problem.goal_test(node.state):
-        return node
-    frontier = deque([node])
-    explored = set()
-    while frontier:
-        node = frontier.popleft()
-        explored.add(node.state)
-        for child in node.expand(problem):
-            if child.state not in explored and child not in frontier:
-                if problem.goal_test(child.state):
-                    return child
-                frontier.append(child)
-    return None
-
-
-
-
-maze = create_maze()
-print_maze(maze)'''
-
-
+if path:
+    print("Path found:")
+    for position in path:
+        print(position)
+else:
+    print("No path found.")
 
