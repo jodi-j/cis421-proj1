7f80492b4805ac6d8dfb3e3017f14885eb1d9dee
jodi <128635337+jodi-j@users.noreply.github.com>
2024-02-07 20:44:21 -0500

    path visualizations

--- grid_maze.py
+++ grid_maze.py
@@ -1,5 +1,10 @@
 import random
 from collections import deque
+import heapq
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib.pyplot import figure
 
 class Maze:
     def __init__(self, size, density):
@@ -70,6 +75,16 @@ class Maze:
                 frontier.append(((row, col + 1), path + [current]))
                 frontier.append(((row, col - 1), path + [current]))
         return None
+    
+    def maze_to_binary(self):
+        binary_maze = [[1 if cell == 'x' else 0 for cell in row] for row in self.maze]
+        for i in range(self.size):
+            for j in range(self.size):
+                if (i, j) == self.start:
+                    binary_maze[i][j] = -1
+                elif (i, j) == self.goal:
+                    binary_maze[i][j] = 2
+        return binary_maze
 
 test = Maze(size=10, density=0.3)
 test.create_maze()
@@ -87,3 +102,52 @@ if path and path2:
         print(pos)
 else:
     print("No path found.")
+    
+#transform maze into binary form for matplotlib visualization
+binary_array = test.maze_to_binary()
+BFS_array = binary_array
+DFS_array = binary_array
+A_array = binary_array
+
+#BFS visualization
+fig_BFS, ax_BFS = plt.subplots(figsize=(12, 12))
+#set values for path cells
+if path:
+    for position in path:
+        row, col = position
+        if BFS_array[row][col] != -1 and BFS_array[row][col] != 2:
+            BFS_array[row][col] = -2    
+color_map = plt.cm.tab20b
+color_map.set_over('blue')
+color_map.set_under('green')
+ax_BFS.imshow(BFS_array, cmap=color_map, interpolation='nearest', vmin=-2, vmax=2)
+ax_BFS.set_title("Maze Visualization - BFS")
+bounds= [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]
+norm = mcolors.BoundaryNorm(bounds, color_map.N)
+cbar_BFS = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=color_map), ax=ax_BFS, ticks=[-2, -1, 0, 1, 2])
+cbar_BFS.ax.set_yticklabels(['Agent Path', 'Start', 'Open Path', 'Obstacle', 'Goal'])
+
+#DFS visualization
+fig_DFS, ax_DFS = plt.subplots(figsize=(12, 12))
+#set values for path cells
+if path2:
+    for position in path2:
+        row, col = position
+        if DFS_array[row][col] != -1 and DFS_array[row][col] != 2:
+            DFS_array[row][col] = -2    
+color_map = plt.cm.tab20b
+color_map.set_over('blue')
+color_map.set_under('green')
+ax_DFS.imshow(DFS_array, cmap=color_map, interpolation='nearest', vmin=-2, vmax=2)
+ax_DFS.set_title("Maze Visualization - DFS")
+bounds= [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]
+norm = mcolors.BoundaryNorm(bounds, color_map.N)
+cbar_DFS = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=color_map), ax=ax_DFS, ticks=[-2, -1, 0, 1, 2])
+cbar_DFS.ax.set_yticklabels(['Agent Path', 'Start', 'Open Path', 'Obstacle', 'Goal'])
+
+#A* visualization
+
+
+
+
+plt.show()
