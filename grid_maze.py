7033d09b9cbe62dfa74b6355ae87f807d5464c2e
shams-ahson <sahson456@gmail.com>
2024-02-11 18:25:55 -0500

    fix

--- grid_maze.py
+++ grid_maze.py
@@ -197,7 +197,7 @@ class Maze: #defines a Maze object
     
 
 
-def main()
+def main():
     '''The main function calls each algorithm and builds the visualization.'''
 
     def print_path(print_path):
